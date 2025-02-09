97cf0f92bf86d5ce8fedadaeb4a8371b8510aad7
Priti <pb131019993@gmail.com>
2025-02-09 21:45:56 +0530

    FirstCommit

--- app/src/main/java/com/example/moviesphere/MainActivity.kt
+++ app/src/main/java/com/example/moviesphere/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
