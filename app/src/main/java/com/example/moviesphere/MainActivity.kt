3bc5701eb2abead38398a9053f9d6a2da0999684
Priti <pb131019993@gmail.com>
2025-02-09 21:44:21 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/moviesphere/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.moviesphere
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        Log.i("MyTag", "Testing")
+    }
+}
