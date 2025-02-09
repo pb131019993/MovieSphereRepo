cf4cd22123e67d15607bbcb81b3381cfd5858894
Priti <pb131019993@gmail.com>
2025-02-09 21:46:57 +0530

    Resolved Issue

--- app/src/main/java/com/example/moviesphere/MainActivity.kt
+++ app/src/main/java/com/example/moviesphere/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
